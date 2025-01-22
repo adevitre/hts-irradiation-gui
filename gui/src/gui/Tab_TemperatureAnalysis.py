944c0782a55f5b0d2c9a93c06978e3fbfcef79ea
adevitre <alexisrdevitre@gmail.com>
2025-01-21 23:43:45 -0500

    minor changes

--- gui/src/gui/Tab_TemperatureAnalysis.py
+++ gui/src/gui/Tab_TemperatureAnalysis.py
@@ -1,9 +1,8 @@
-import os, numpy, time, re, datetime, pyqtgraph
+import os, sys, numpy, time, re, datetime, pyqtgraph
 
 from configure import load_json
-from progresslabel import ProgressLabel
 from plottingArea import MeasurePlot
-from fittingFunctions import powerLaw
+
 
 from PyQt5.QtWidgets import QWidget, QPushButton, QComboBox, QLabel, QGridLayout, QVBoxLayout, QDoubleSpinBox, QSpinBox, QFileDialog, QMessageBox, QInputDialog
 from PyQt5.QtGui import QFont
@@ -53,35 +52,11 @@ class Tab_TemperatureAnalysis(QWidget):
         '''
         
         '''
-        filepaths = QFileDialog.getOpenFileNames(self, 'Select IV curves to process', self.parent.dm.save_directory+'/Ic')[0]
-        for path in filepaths:
-            try:
-                i, v, ic, n, temp, tag = [], [], 1., 1., 1.,'fail' 
-                i, v = numpy.genfromtxt((line  for line  in open(path, 'rb') if line[0] != '#'), usecols=[2, 3], unpack=True)
-                if v[numpy.argmax(numpy.abs(v))] < 0:
-                    v *= -1 # fix for data where sample was reverse biased.
+        #filepaths = QFileDialog.getOpenFileNames(self, 'Select IV curves to process', self.parent.dm.save_directory+'/Ic')[0]
+        #data = getIcT(filepaths)[2]
+        #for index, row in data.iterrows():
+        #    pen = pyqtgraph.mkPen(color=(int(numpy.random.rand()*255), int(numpy.random.rand()*255), int(numpy.random.rand()*255)), width=3, symbol='+', symbolSize=5)
+        #    self.plottingArea.plotData(row.sampleT, row.ic, name='{}'.format(row.fname), pen=pen)
 
-                with open(path) as f:
-                    fit_parameters = f.readline().split()
-                    ic, n, temp, tag = float(fit_parameters[3]), float(fit_parameters[7]), float(fit_parameters[-3]), fit_parameters[-1]
-                    f.close()
-                    
-            except ValueError as e:
-                QMessageBox.warning(self, 'File does not contain an Ic Measurement', 'Did you forget your ADHD medication today? ', e)
-
-            except UnboundLocalError as e:
-                print('Crappy file!', e)
-
-            except Exception as e:
-                ic, n, tag = self.parent.dm.fitIcMeasurement(i, v), 'legacy'
-
-            finally:
-                pen = pyqtgraph.mkPen(color=(int(numpy.random.rand()*255), int(numpy.random.rand()*255), int(numpy.random.rand()*255)), width=3, symbol='+', symbolSize=5)
-                self.plottingArea.plotData(i, v*1e6, name='T = {:4.2f} {}'.format(temp, tag), pen=pen)
-                
-                fitParameters = [ic, n]
-                if numpy.isfinite(fitParameters).all():
-                    self.plottingArea.plotFit(xvalues=i, fitParameters=fitParameters, pen=pen)
-        
     def clearPlot(self):
         self.plottingArea.clear()
